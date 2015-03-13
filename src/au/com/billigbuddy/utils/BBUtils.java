b6e5bc31a749e60c917ccc82ae1afd63d4021f92
Edicson Morales <edicsonm@gmail.com>
2015-03-13 18:51:45 +1100

    Respaldo al 03-13-15 18:51:45

--- src/au/com/billigbuddy/utils/BBUtils.java
+++ src/au/com/billigbuddy/utils/BBUtils.java
@@ -2,6 +2,9 @@ package au.com.billigbuddy.utils;
 
 import java.text.DecimalFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.MissingResourceException;
 
 
@@ -55,6 +58,47 @@ public class BBUtils {
 	public static String booleanToString(boolean val){
 		if(val)return "0";
 		return "1";
+	}
+	
+	
+	public static String getCurrentDate(int format){
+		return getDateFormat(format).format(Calendar.getInstance().getTime()); 
+	}
+	
+	/*Obtiene una fecha restandole o sumandole dias a partitr de dia de hoy*/
+	public static String getCurrentDate(int format, int days){
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_YEAR, days);
+		Date date = cal.getTime();
+		return getDateFormat(format).format(date); 
+	}
+	
+	public static SimpleDateFormat getDateFormat(int format){
+		SimpleDateFormat simpleDateFormat = null;
+		switch (format) {
+		case 1:
+			simpleDateFormat =  new SimpleDateFormat("MM-dd-yyyy");
+			break;
+		case 2:
+			simpleDateFormat =  new SimpleDateFormat("yyyy-MM-dd");
+			break;
+		case 3:
+			simpleDateFormat =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			break;
+		case 4:
+			simpleDateFormat =  new SimpleDateFormat("MM-dd");
+			break;
+		case 5:
+			simpleDateFormat =  new SimpleDateFormat("dd-MM-yyyy");
+			break;
+		case 6:
+			simpleDateFormat =  new SimpleDateFormat("MM/dd/yyyy");
+			break;
+		default:
+			simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+			break;
+		}
+		return simpleDateFormat;
 	}
 	
 }
